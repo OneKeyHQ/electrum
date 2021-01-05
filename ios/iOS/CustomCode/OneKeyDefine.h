35cb729cfd7be670f960d9624bd6b6c579908ab8
xiaoliang <137569752@qq.com>
2021-01-05 19:26:17 +0800

    Initialize project

---
+++ ios/iOS/CustomCode/OneKeyDefine.h
@@ -0,0 +1,19 @@
+//
+//  OneKeyDefine.h
+//  OneKey
+//
+//  Created by xiaoliang on 2020/11/11.
+//  Copyright © 2020 OneKey. All rights reserved..
+//
+
+#ifndef OneKeyDefine_h
+#define OneKeyDefine_h
+
+#define kTheServiceAgreement        @"https://onekey.zendesk.com/hc/articles/360002014776"
+#define kPrivacyAgreement           @"https://onekey.zendesk.com/hc/articles/360002003315"
+#define kSavedUUIDForLastBlueTooth  @"kSavedUUIDForLastBlueTooth"
+
+#define NetTypeMain                     @"set_mainnet"
+#define NetTypeTest                     @"set_testnet"
+
+#endif /* OneKeyDefine_h */
